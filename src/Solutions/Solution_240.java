dbea10cdaab912d619da7c495e801f2406c45b0b
chandx <silencer0x0@gmail.com>
2018-07-19 14:48:35 -0400

    solve 240

---
+++ src/Solutions/Solution_240.java
@@ -0,0 +1,14 @@
+package Solutions;
+
+public class Solution_240 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row= matrix.length-1;
+        int col= 0;
+        while (row >= 0 && col < matrix[0].length){
+            if (target> matrix[row][col]) col++;
+            else if (target< matrix[row][col]) row--;
+            else return true;
+        }
+        return false;
+    }
+}
