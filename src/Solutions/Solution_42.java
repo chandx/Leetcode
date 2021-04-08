c66d79b39ccc872e74ac33af4d724e8fd484ca69
chandx <silencer0x0@gmail.com>
2021-04-08 16:18:17 +0800

    before back to china

--- src/Solutions/Solution_42.java
+++ src/Solutions/Solution_42.java
@@ -1,5 +1,7 @@
 package Solutions;
 
+import java.util.*;
+
 public class Solution_42 {
     public int trap(int[] height){
         int[] left= new int[height.length];
@@ -21,5 +23,74 @@ public class Solution_42 {
             result= result+ Math.min(left[i], right[i])- height[i];
         }
         return result;
+    }
+}
+//class Solution {
+//    HashMap<Integer,List<Integer>> adjlist;
+//
+//    public int findClosestLeaf(TreeNode root, int k) {
+//        adjlist= new HashMap<>();
+//        Queue<Integer> q= new LinkedList<>();
+//        Set<Integer> seen= new HashSet<>();
+//        adjList(root);
+//        q.add(k);
+//        while (!q.isEmpty()){
+//            int current= q.poll();
+//            seen.add(current);
+//            if (adjlist.get(current).size()==1) return current;
+//            for (int child: adjlist.get(current)){
+//                if (!seen.contains(child)) q.add(child);
+//            }
+//        }
+//    }
+//
+//    private void adjList(TreeNode root){
+//        if (root!= null){
+//            if(root.left!= null){
+//                adjlist.computeIfAbsent(root.val, k-> new ArrayList<>()).add(root.left.val);
+//                adjlist.computeIfAbsent(root.left.val, k-> new ArrayList<>()).add(root.val);
+//                adjList(root.left);
+//            }
+//            if(root.right!= null){
+//                adjlist.computeIfAbsent()
+//                adjlist.computeIfAbsent(root.val, k-> new ArrayList<>()).add(root.right.val);
+//                adjlist.computeIfAbsent(root.right.val, k-> new ArrayList<>()).add(root.val);
+//                adjList(root.right);
+//            }
+//        }
+//    }
+//
+//    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+//        //convert flights to adjacency list;
+//        HashMap<Integer, LinkedList<int[]>> fchart= new HashMap<>();
+//        for(int[] flight: flights){
+//            int[] temp= new int[2];
+//            temp[0]= flight[1];
+//            temp[1]= flight[2];
+//            fchart.computeIfAbsent(flight[0], k-> new LinkedList<int[]>()).add(temp);
+//        }
+//
+//        //dijkstra
+//        PriorityQueue<int[]> minHeap = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
+//        minHeap.offer(new int[]{src, 0, 0});
+//
+//        while(!minHeap.isEmpty()){
+//            minHeap.poll();
+//        }
+//    }
+//
+//}
+
+class LRUCache<K, V> extends LinkedHashMap<K, V>{
+    private final int CACHE_SIZE;
+
+    LRUCache(int cache_size) {
+        super((int) Math.ceil(cache_size / 0.75) + 1, 0.75f, true);
+        CACHE_SIZE = cache_size;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return super.removeEldestEntry(eldest);
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,7 @@
 import Solutions.*;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Pattern;
 
 public class Test {
@@ -11,5 +10,57 @@ public class Test {
         int[] nums= new int[]{-2,1,-3,4,-1,2,1,-5,4};
         Solution_53 bench= new Solution_53();
         System.out.println(bench.maxSubArray(nums));
+        String st= "sadfasdfe";
+        st.concat();
+
+        ConcurrentHashMap
+
+    }
+
+    public String removeDuplicates(String S) {
+        Stack<Character> st =new Stack<>();
+        boolean trigger= false;
+        for(char c: S.toCharArray()){
+            if(st.isEmpty()) {
+                st.push(c);
+                trigger= false;
+            }
+            else{
+                if(c== st.peek()){
+                    trigger= true;
+                }
+                else{
+                    if(trigger) st.pop();
+                    trigger= false;
+                    st.push(c);
+                }
+            }
+        }
+        StringBuilder sb= new StringBuilder();
+        whlie(!st.isEmpty()){
+            sb.insert(0, st.pop());
+        }
+        return sb.toString();
+    }d
+
+    public static int fillTheTruck(int num, int[] boxes, int unitSize, int[] unitsPerBox,  int truckSize) {
+        // WRITE YOUR BRILLIANT CODE HERE
+        int boxTotal= 0;
+        int units= 0;
+        TreeMap<Integer, Integer> tMap= new TreeMap<>(Collections.reverseOrder());
+        for(int i= 0; i< boxes.length; i++){
+            tMap.put(unitsPerBox[i], boxes[i]);
+        }
+        while(tMap.size()>0 && boxTotal< truckSize){
+            Map.Entry<Integer, Integer> c= tMap.pollFirstEntry();
+            if(boxTotal+c.getValue()>= truckSize){
+                return units+(truckSize-boxTotal)*c.getKey();
+            }
+            else{
+                boxTotal= boxTotal+ c.getValue();
+                units= units+(c.getKey()*c.getValue());
+            }
+        }
+        return units;
     }
 }
